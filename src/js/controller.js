64c1cbcb7cecd8cd466033369d612d6a6ba620f4
Alan Cashman <nocabbage@gmail.com>
2022-12-26 14:00:05 -0800

    removed new-feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -131,11 +131,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
-
-const newFeature = function () {
-  console.log('Welcome to the application!');
-};
